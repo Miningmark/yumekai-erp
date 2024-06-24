6eeec528e3bbb1d09f55af68ad596d028edce0c2
Markus <markus.sibbe@t-online.de>
2024-06-24 16:31:39 +0200

    test no-referrer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { GlobalStyles } from "@/lib/global-styles";
 import StyledComponentsRegistry from "@/lib/styled-components-registry";
 import MenuLayout from "@/components/menu/MenuLayout";
+import Head from "next/head";
 
 export const metadata = {
   title: "YumeKai Planungsboard",
@@ -10,6 +11,11 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="de">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+        {/* Weitere Meta-Tags und Header-Inhalte hier einfügen */}
+      </Head>
       <body>
         <GlobalStyles />
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
